d213a419e2b6a33eff0cb824c97cc03fc9089416
cpb100 <cpbailey025@gmail.com>
2021-07-21 22:35:50 +0100

    Added a little bit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
+//TODO: Create buttons and counters for different sale areas
+//Potential TODO: Create a way to store and manage sales from different weeks/days
+
 void main() {
-  
+
 }
 
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'O2 Sales Counter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+
+    );
+  }
+}
 
